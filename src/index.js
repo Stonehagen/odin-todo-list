bf443db065728252c0c7bd000a1172c752b3cef0
Stonehagen <steinhagen@mailbox.org>
2023-01-11 13:52:35 +0100

    renam the modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
-import pageLoad from './modules/page-load';
-import { projectList, Project } from './modules/project-list';
+import pageLoad from './modules/pageLoad';
+import { projectList, Project } from './modules/project';
 
 const contentContainer = document.querySelector('#content');
 
